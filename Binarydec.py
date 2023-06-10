f2fc93be7f563bd06484cb58fde16aeeaed56f7f
Shadin mc <95973217+shadinmc@users.noreply.github.com>
2023-06-10 08:57:57 +0530

    Create Binarydec.py

---
+++ Binarydec.py
@@ -0,0 +1,15 @@
+bnrydec.py
+print("Enter the Binary Number: ", end="")
+bnum = int(input())
+
+dnum = 0
+m = 1
+blen = len(str(bnum))
+
+for k in range(blen):
+    rem = bnum%10
+    dnum = dnum + (rem * m)
+    m = m * 2
+    bnum = int(bnum/10)
+
+print("\nEquivalent Decimal Value = ", dnum)
