9de6ddf490f13e0a18b8e8848366f3f1f7e759f5
Shadin mc <95973217+shadinmc@users.noreply.github.com>
2023-06-10 08:46:37 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+fibnocci.py
+def fib(num):
+	n1, n2 = 0, 1
+	print("Fibonacci Series:", n1, n2, end=" ")
+	for i in range(2, num):
+	    n3 = n1 + n2
+	    n1 = n2
+	    n2 = n3
+	    print(n3, end=" ")
+
+	print()
+
+num = int(input("Enter the number of fibonacci series"))
+fib(num)	
